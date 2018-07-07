3553743278f040ac126427a7417ebd1168208353
Arsalaan Ansari <arsalaan.ansari@Arsalaans-MacBook-Pro.local>
2018-07-07 17:01:15 -0400

    Added rehive package to app

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,15 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
+const Rehive = require('rehive');
+
+var config = {
+    apiVersion: 3, 
+    apiToken: process.env.RAPI
+}
+
+const myrehive = new Rehive(config);
+
 
 mongoose.Promise = require('bluebird');
 
@@ -24,6 +33,8 @@ mongoose.connect(URL)
 
 
 
+
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+app.get('/app')
 app.listen(8080, () => console.log('Listening on port 8080!'));
