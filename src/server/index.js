5754ce897239a44ae1b79b051bfb4e6e2e8a53a8
Arsalaan Ansari <arsalaan.ansari@Arsalaans-MacBook-Pro.local>
2018-07-07 13:45:36 -0400

    Added modules for monggodb dependencies

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const os = require('os');
-
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
+
+mongoose.Promise = require('bluebird');
+
+const DBUSER = process.env.DBUSER
+const DBPASS = process.env.DBPASS
+
+/*mongoose.connect('mongodb://<uname>:<pwd>@ds139322.mlab.com:39322/aufinancex')
+    .then(() => { // if all is ok we will be here
+      console.log('Start');
+    })
+    .catch(err => { // if error we will be here
+        console.error('App starting error:', err.stack);
+        process.exit(1);
+    });
+
+*/
 
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
