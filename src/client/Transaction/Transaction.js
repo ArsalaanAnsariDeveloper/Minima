4ceb2935ef216cf47ba9f3e017cadd67f166dbbe
kevinl94303 <kevinl94303spam@gmail.com>
2018-07-08 14:12:01 -0400

    updates styles on transactions page

--- src/client/Transaction/Transaction.js
+++ src/client/Transaction/Transaction.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Transaction.css'
+import Navbar from '../Homepage/Navbar/Navbar'
 import Button from '../Button/Button'
 
 export default class Transaction extends Component {
@@ -61,32 +62,39 @@ export default class Transaction extends Component {
 
     render(){
         return <div>
+            <Navbar userID = {this.props.userID}/>
+            <div className="transaction-wrapper">
                 <div className = 'transaction-title'>
                     Create Transaction
                 </div>
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                Title
-                    <input type="text" defaultValue={this.state.title} onChange={this.handleTitleChange} />
-                </label>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                    Title<br/>
+                        <input type="text" defaultValue={this.state.title} onChange={this.handleTitleChange} />
+                    </label><br/>
 
-                <label>
-                Expense Category
-                    <input type="text" defaultValue={this.state.category} onChange={this.handleCategoryChange} />
-                </label>
+                    <label>
+                    Expense Category<br/>
+                        <input type="text" defaultValue={this.state.category} onChange={this.handleCategoryChange} />
+                    </label><br/>
 
-                <label>
-                Amount ($)
-                    <input type="number" defaultValue={this.state.amt} onChange={this.handleAmtChange} />
-                </label>
+                    <label>
+                    Amount ($)<br/>
+                        <input type="number" defaultValue={this.state.amt} onChange={this.handleAmtChange} />
+                    </label><br/>
 
-                <label>
-                Recipient
-                    <input type="text" defaultValue={this.state.recipient} onChange={this.handleRecipientChange} />
-                </label>
+                    <label>
+                    Recipient<br/>
+                        <input type="text" defaultValue={this.state.recipient} onChange={this.handleRecipientChange} />
+                    </label><br/>
 
-                <input type="submit" value="Submit" />
-            </form>
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
         </div>
     }
+}
+
+Transaction.defaultProps = {
+    userID:"4W442QBG9Q"
 }
