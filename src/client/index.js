0adf4c5a7f1131adf89739bd84400f650b4ac027
Arsalaan Ansari <arsalaan.ansari@Arsalaans-MacBook-Pro.local>
2018-07-08 10:20:40 -0400

    Added basic routing structure to app

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Homepage from './Homepage/Homepage';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(<Homepage />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+      <div>
+        <Route exact path="/" component={Homepage} />
+      </div>
+  </Router> 
+    , document.getElementById('root'));
