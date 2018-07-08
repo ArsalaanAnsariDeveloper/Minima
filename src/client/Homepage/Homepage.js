2592cf4d723b6a1e4dead26bf084d7975c934c1b
kevinl94303 <kevinl94303spam@gmail.com>
2018-07-08 09:58:52 -0400

    you bum

---
+++ src/client/Homepage/Homepage.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import BudgetPie from './BudgetPie/BudgetPie'
+import Dashboard from './Dashboard/Dashboard'
+import Navbar from './Navbar/Navbar'
+import './Homepage.css';
+
+export default class Homepage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <div className = "navbar">
+        <Navbar/>
+        </div>
+        <div className = "dashComponent">
+        <Dashboard/>
+        </div>
+
+        <div className = "chartComponent">
+          <BudgetPie 
+            outerRadius={100}
+            innerRadius={75}
+            title="My Budget"/>
+        </div>
+      </div>
+    );
+  }
+}
